582e52c0c3a0c2408f5fc063d2147dc325d2bb94
YonatanKoifman <yonatanko@campus.technion.ac.il>
2022-11-25 15:52:10 +0200

    finished knn F1 score: 0.6906 model: 'glove-wiki-gigaword-100' classifier: knn (k = 5)

--- main.py
+++ main.py
@@ -5,6 +5,8 @@ from gensim.models import Word2Vec
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.metrics import f1_score
 
+model = downloader.load('glove-wiki-gigaword-100')
+
 
 def load_data(file_path):
     with open(file_path, 'r', encoding='utf-8-sig') as f:
@@ -32,36 +34,62 @@ def load_data(file_path):
                 sentence_tags.append(tag)
                 words.append(word)
                 word_to_label[word] = tag
-                sentence.append(word)
-                sentence_tags.append(tag)
-                words.append(word)
-                word_to_label[word] = tag
 
     return sentences, all_tags, words, word_to_label
+
+
+def build_set(words, model, word_to_label):
+    train_set = []
+    train_tags = []
+    not_appeared_positive = []
+    not_appeared_negative = []
+    avg_positive_vec = np.zeros(100)
+    avg_negative_vec = np.zeros(100)
+    num_known_positive = 0
+    num_known_negative = 0
+    for word in words:
+        if word not in model.key_to_index:
+            if word_to_label[word] == '1':
+                not_appeared_positive.append(word)
+            else:
+                not_appeared_negative.append(word)
+        else:
+            word_vec = model[word]
+
+            if word_to_label[word] == '1':
+                num_known_positive += 1
+                avg_positive_vec = [sum(x) for x in zip(avg_positive_vec, word_vec)]
+            else:
+                num_known_negative += 1
+                avg_negative_vec = [sum(x) for x in zip(avg_negative_vec, word_vec)]
+
+            train_set.append(word_vec)
+            train_tags.append(word_to_label[word])
+
+    for word in not_appeared_positive:
+        train_set.append([x / num_known_positive for x in avg_positive_vec])
+        train_tags.append('1')
+    for word in not_appeared_negative:
+        train_set.append([x / num_known_negative for x in avg_negative_vec])
+        train_tags.append('0')
+
+    return train_set, train_tags
 
 
 def main():
     # Load training data
     sentences, all_tags, words, word_to_label = load_data('train.tagged')
-    all_words = set(words)
+    train_all_words = set(words)
 
     # define word2vec model
-    model = Word2Vec(sentences=sentences, vector_size=10, window=2, min_count=1, workers=4)
+    # model = Word2Vec(sentences=sentences, vector_size=10, window=2, min_count=1, workers=4)
+    # model = downloader.load('word2vec-google-news-300')
+    # model = model.sv
 
     # generate train set
-    train_set = []
-    train_tags = []
-
-    for word in all_words:
-        if word not in model.wv.key_to_index:
-            word_vec = np.random.rand(10)
-        else:
-            word_vec = model.wv[word]
-        train_set.append(word_vec)
-        train_tags.append(word_to_label[word])
-
+    train_set, train_tags = build_set(train_all_words, model, word_to_label)
     # train knn
-    knn = KNeighborsClassifier(n_neighbors=3)
+    knn = KNeighborsClassifier(n_neighbors=5)
     knn.fit(train_set, train_tags)
 
     # load test data
@@ -69,22 +97,11 @@ def main():
     test_all_words = set(test_words)
 
     # generate test set
-    test_set = []
-    test_tags = []
-
-    for word in test_all_words:
-        if word not in model.wv.key_to_index:
-            word_vec = np.random.rand(10)
-        else:
-            word_vec = model.wv[word]
-
-        test_set.append(word_vec)
-        test_tags.append(test_word_to_label[word])
+    test_set, test_tags = build_set(test_all_words, model, test_word_to_label)
 
     # predict
     predictions = knn.predict(test_set)
-    print(set(predictions))
-    print(set(test_tags))
+
     # calculate F1 score
     f1_score_value = f1_score(test_tags, predictions, average='binary', pos_label='1')
     print(f"f1 score: {f1_score_value}")
