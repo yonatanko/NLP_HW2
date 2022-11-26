48ba8ab22443758cfb5c2935e469ef525be49484
YonatanKoifman <yonatanko@campus.technion.ac.il>
2022-11-26 11:33:14 +0200

    working model1 working model2 (need to increase F1 score)
    model 2 output:
    
    Model: glove-wiki-gigaword-50
    Epoch 1 of 5
    ----------
    train Loss: 0.13153828619067792, F1: 0.7227227227227228, accuracy: 0.9574657576315951
    dev Loss: 0.25209989741598854, F1: 0.45639913232104123, accuracy: 0.9203584821712324
    
    Epoch 2 of 5
    ----------
    train Loss: 0.09286184252487052, F1: 0.8108481262327417, accuracy: 0.9705484921073644
    dev Loss: 0.2536438844949675, F1: 0.4317683194383501, accuracy: 0.9176889340875866
    
    Epoch 3 of 5
    ----------
    train Loss: 0.07647753488523835, F1: 0.8310911808669657, accuracy: 0.9739727289478534
    dev Loss: 0.3135473303513664, F1: 0.42052221713238663, accuracy: 0.9195957541473336
    
    Epoch 4 of 5
    ----------
    train Loss: 0.06496713672572454, F1: 0.8504188272836058, accuracy: 0.9769670167680118
    dev Loss: 0.3110217667140122, F1: 0.488482922954726, accuracy: 0.9181338587681942
    
    Epoch 5 of 5
    ----------
    train Loss: 0.056170995493076706, F1: 0.8726303317535543, accuracy: 0.9801916344204902
    dev Loss: 0.2973187362411354, F1: 0.5001909125620465, accuracy: 0.9167990847263713
    
    Max F1: 0.5002
    
    Model: glove-wiki-gigaword-100
    Epoch 1 of 5
    ----------
    train Loss: 0.12145356826853049, F1: 0.7667976612671331, accuracy: 0.9626405011977152
    dev Loss: 0.2835258228898139, F1: 0.44712990936555885, accuracy: 0.9185787834488018
    
    Epoch 2 of 5
    ----------
    train Loss: 0.09293716651310918, F1: 0.8155132192846035, accuracy: 0.9708555985504576
    dev Loss: 0.25938540614394284, F1: 0.4139630390143737, accuracy: 0.9092989258246996
    
    Epoch 3 of 5
    ----------
    train Loss: 0.0699113784030418, F1: 0.847770575525437, accuracy: 0.9761992506602789
    dev Loss: 0.32143045909843987, F1: 0.4865706500583885, accuracy: 0.9161634780397889
    
    Epoch 4 of 5
    ----------
    train Loss: 0.0533622306110295, F1: 0.8809291430802264, accuracy: 0.9812665069713162
    dev Loss: 0.3868000437682241, F1: 0.3825, accuracy: 0.9058030890484968
    
    Epoch 5 of 5
    ----------
    train Loss: 0.04326550812703037, F1: 0.9030755797031145, accuracy: 0.9846600331674958
    dev Loss: 0.3933919396966595, F1: 0.4877505567928731, accuracy: 0.9122862772516367
    
    Max F1: 0.4878
    
    Model: glove-wiki-gigaword-200
    Epoch 1 of 5
    ----------
    train Loss: 0.1224605948965422, F1: 0.7671575705072672, accuracy: 0.9628554757078803
    dev Loss: 0.2665638328063256, F1: 0.42396694214876035, accuracy: 0.9113964278904214
    
    Epoch 2 of 5
    ----------
    train Loss: 0.09005350867672673, F1: 0.818411342008157, accuracy: 0.971285547570788
    dev Loss: 0.29719989555752985, F1: 0.3975155279503106, accuracy: 0.9075192271022691
    
    Epoch 3 of 5
    ----------
    train Loss: 0.06664915475089826, F1: 0.8575334439996093, accuracy: 0.9775965849763528
    dev Loss: 0.3302716880149348, F1: 0.40066225165562913, accuracy: 0.9079641517828767
    
    Epoch 4 of 5
    ----------
    train Loss: 0.049895479711142936, F1: 0.890505285617302, accuracy: 0.9826638412873903
    dev Loss: 0.42137408480379784, F1: 0.38519764507989906, accuracy: 0.9070743024216614
    
    Epoch 5 of 5
    ----------
    train Loss: 0.03760377685954611, F1: 0.9191119691119691, accuracy: 0.9871322400343959
    dev Loss: 0.474120910653692, F1: 0.40148392415498757, accuracy: 0.9077099091082438
    
    Max F1: 0.4240
    
    Process finished with exit code 0

--- model2.py
+++ model2.py
@@ -1,14 +1,14 @@
-import gensim
+from torch import nn
+import torch
+from torch.utils.data import DataLoader
+from sklearn.metrics import f1_score, accuracy_score
 from gensim import downloader
 import numpy as np
-from gensim.models import Word2Vec
-from sklearn.metrics import f1_score
+import re
 from torch.optim import Adam
-import FFnn
 
 def load_data(file_path):
-
-    with open(file_path, 'r', encoding='utf-8') as f:
+    with open(file_path, 'r', encoding='utf-8-sig') as f:
         lines = f.readlines()
 
     sentences = []
@@ -38,8 +38,8 @@ def load_data(file_path):
 
 
 def build_set(words, model, word_to_label, length):
-    train_set = []
-    train_tags = []
+    set_data = []
+    set_tags = []
     not_appeared_positive = []
     not_appeared_negative = []
     avg_positive_vec = np.zeros(length)
@@ -62,44 +62,128 @@ def build_set(words, model, word_to_label, length):
                 num_known_negative += 1
                 avg_negative_vec = [sum(x) for x in zip(avg_negative_vec, word_vec)]
 
-            train_set.append(word_vec)
-            train_tags.append(word_to_label[word])
+            set_data.append(word_vec)
+            set_tags.append(word_to_label[word])
 
     for word in not_appeared_positive:
-        train_set.append([x / num_known_positive for x in avg_positive_vec])
-        train_tags.append('1')
+        set_data.append([x / num_known_positive for x in avg_positive_vec])
+        set_tags.append('1')
+
+    # this is the part I dropped yesterday, and it increased the F1 score
     for word in not_appeared_negative:
-        train_set.append([x / num_known_negative for x in avg_negative_vec])
-        train_tags.append('0')
+        set_data.append([x / num_known_negative for x in avg_negative_vec])
+        set_tags.append('0')
 
-    return train_set, train_tags
+    return set_data, set_tags
 
 
-def train_and_predict(model, k, length):
-    # Load training data
-    sentences, all_tags, words, word_to_label = load_data('train.tagged')
-    train_all_words = set(words)
-    train_set, train_tags = build_set(train_all_words, model, word_to_label, length)
+class NerNN(nn.Module):
+    def __init__(self, vec_dim, num_classes, hidden_dim=100):
+        super(NerNN, self).__init__()
+        self.first_layer = nn.Linear(vec_dim, hidden_dim)
+        self.second_layer = nn.Linear(hidden_dim, num_classes)
+        self.activation = nn.Tanh()
+        self.loss = nn.CrossEntropyLoss()
 
-    # Load test data
-    test_sentences, test_tags, test_words, test_word_to_label = load_data('dev.tagged')
-    test_all_words = set(test_words)
-    test_set, test_tags = build_set(test_all_words, model, test_word_to_label, length)
+    def forward(self, input_ids, labels=None):
+        x = self.first_layer(input_ids)
+        x = self.activation(x)
+        x = self.second_layer(x)
+        if labels is None:
+            return x, None
+        loss = self.loss(x, labels)
+        return x, loss
 
-    datasets = {'train': (train_set, train_tags), 'test': (test_set, test_tags)}
-    # train model with FF neural network
 
-    # predict
-    predictions = 0
+class NerDataset(torch.utils.data.Dataset):
+    def __init__(self, file_path, tokenizer, model, model_name):
+        self.file_path = file_path
+        self.sentences, _, words, word_to_label = load_data(file_path)
+        # flatten the list of lists
+        self.vector_dim = int(re.findall(r'\d+', model_name)[-1])
+        self.tokenized_sen, self.labels = build_set(words, model, word_to_label, self.vector_dim)
+        self.tokenized_sen = np.stack(self.tokenized_sen)
+        self.tags_to_idx = {tag: idx for idx, tag in enumerate(sorted(list(set(self.labels))))}
+        self.idx_to_tags = {idx: tag for idx, tag in self.tags_to_idx.items()}
 
-    # calculate F1 score
-    f1_score_value = f1_score(test_tags, predictions, average='binary', pos_label='1')
-    print(f"f1 score: {f1_score_value}")
+    def __len__(self):
+        return len(self.labels)
+
+    def __getitem__(self, idx):
+        cur_sen = self.tokenized_sen[idx]
+        cur_sen = torch.FloatTensor(cur_sen.squeeze())
+        label = self.labels[idx]
+        label = self.tags_to_idx[label]
+        return {"input_ids": cur_sen, "labels": label}
+
+
+def train(model, data_sets, optimizer, num_epochs: int, batch_size=16):
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    data_loaders = {"train": DataLoader(data_sets["train"], batch_size=batch_size, shuffle=True, num_workers=4),
+                    "dev": DataLoader(data_sets["dev"], batch_size=batch_size, shuffle=False, num_workers=4)}
+
+    model.to(device)
+    max_f1 = 0
+
+    for epoch in range(num_epochs):
+        print(f"Epoch {epoch + 1} of {num_epochs}")
+        print("-" * 10)
+        for phase in ["train", "dev"]:
+            if phase == "train":
+                model.train()
+            else:
+                model.eval()
+
+            running_loss = 0.0
+            labels, preds = [], []
+            for batch in data_loaders[phase]:
+                batch_size = 0
+                for k, v in batch.items():
+                    batch[k] = v.to(device)
+                    batch_size = v.shape[0]
+                optimizer.zero_grad()
+                if phase == "train":
+                    outputs, loss = model(**batch)
+                    loss.backward()
+                    optimizer.step()
+                else:
+                    with torch.no_grad():
+                        outputs, loss = model(**batch)
+
+                pred = outputs.argmax(dim=-1).clone().detach().cpu()
+                labels += batch["labels"].cpu().view(-1).tolist()
+                preds += pred.view(-1).tolist()
+                running_loss += loss.item() * batch_size
+
+            epoch_loss = running_loss / len(data_sets[phase])
+            epoch_f1 = f1_score(labels, preds)
+            epoch_accuracy = accuracy_score(labels, preds)
+
+            print(f"{phase} Loss: {epoch_loss}, F1: {epoch_f1}, accuracy: {epoch_accuracy}")
+
+            # update max f1 score
+            if phase == "dev" and epoch_f1 > max_f1:
+                max_f1 = epoch_f1
+                with open("model.pkl", "wb") as f:
+                    torch.save(model, f)
+        print()
+
+    print(f"Max F1: {max_f1:.4f}")
 
 
 def main():
-    pass
+    for model_name in ["glove-wiki-gigaword-50", "glove-wiki-gigaword-100", "glove-wiki-gigaword-200"]:
+        model = downloader.load(model_name)
+        print(f"Model: {model_name}")
+        train_set = NerDataset("train.tagged", None, model, model_name)
+        test_set = NerDataset("dev.tagged", None, model, model_name)
+        nn_model = NerNN(num_classes=2, vec_dim=train_set.vector_dim)
+        optimizer = Adam(params=nn_model.parameters())
+        datasets = {"train": train_set, "dev": test_set}
+        train(model=nn_model, data_sets=datasets, optimizer=optimizer, num_epochs=5)
+        print()
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
+
