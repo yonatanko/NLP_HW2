96f4a2fc20cae2d4231629679b4860dc7aa3f3e1
YonatanKoifman <yonatanko@campus.technion.ac.il>
2022-12-07 15:52:51 +0200

    model 1 finished. model 2: concatenating word2vec and glove vectors

--- model2.py
+++ model2.py
@@ -5,7 +5,7 @@ from sklearn.metrics import f1_score, accuracy_score
 from gensim import downloader
 import numpy as np
 import re
-from torch.optim import Adam
+from torch.optim import Adam, Adagrad
 
 def load_data(file_path):
     with open(file_path, 'r', encoding='utf-8-sig') as f:
@@ -37,16 +37,22 @@ def load_data(file_path):
     return sentences, all_tags, words, word_to_label
 
 
-def build_set(words, model, word_to_label, length):
+def build_set(words, model1,model2, word_to_label, length):
     set_data = []
     set_tags = []
 
     for word in words:
-        if word not in model.key_to_index:
-            word_vec = np.zeros(length)
+        if word not in model1.key_to_index:
+            word_vec_1 = np.zeros(model1.vector_size)
         else:
-            word_vec = model[word]
+            word_vec_1 = model1[word]
 
+        if word not in model2.key_to_index:
+            word_vec_2 = np.zeros(model2.vector_size)
+        else:
+            word_vec_2 = model2[word]
+
+        word_vec = np.concatenate((word_vec_2, word_vec_1))
         set_data.append(word_vec)
         set_tags.append(word_to_label[word])
 
@@ -57,7 +63,8 @@ class NerNN(nn.Module):
     def __init__(self, vec_dim, num_classes, hidden_dim=100):
         super(NerNN, self).__init__()
         self.first_layer = nn.Linear(vec_dim, hidden_dim)
-        self.second_layer = nn.Linear(hidden_dim, num_classes)
+        self.second_layer = nn.Linear(hidden_dim, hidden_dim)
+        self.third_layer = nn.Linear(hidden_dim, num_classes)
         self.activation = nn.Tanh()
         self.loss = nn.CrossEntropyLoss()
 
@@ -65,6 +72,9 @@ class NerNN(nn.Module):
         x = self.first_layer(input_ids)
         x = self.activation(x)
         x = self.second_layer(x)
+        x = self.activation(x)
+        x = self.third_layer(x)
+
         if labels is None:
             return x, None
         loss = self.loss(x, labels)
@@ -72,12 +82,12 @@ class NerNN(nn.Module):
 
 
 class NerDataset(torch.utils.data.Dataset):
-    def __init__(self, file_path, tokenizer, model, model_name):
+    def __init__(self, file_path, tokenizer, model1, model2, model1_name, model2_name):
         self.file_path = file_path
         self.sentences, _, words, word_to_label = load_data(file_path)
         # flatten the list of lists
-        self.vector_dim = int(re.findall(r'\d+', model_name)[-1])
-        self.tokenized_sen, self.labels = build_set(words, model, word_to_label, self.vector_dim)
+        self.vector_dim = int(re.findall(r'\d+', model1_name)[-1]) + int(re.findall(r'\d+', model2_name)[-1])
+        self.tokenized_sen, self.labels = build_set(words, model1,model2, word_to_label, self.vector_dim)
         self.tokenized_sen = np.stack(self.tokenized_sen)
         self.tags_to_idx = {tag: idx for idx, tag in enumerate(sorted(list(set(self.labels))))}
         self.idx_to_tags = {idx: tag for idx, tag in self.tags_to_idx.items()}
@@ -132,7 +142,6 @@ def train(model, data_sets, optimizer, num_epochs: int, batch_size=16):
                 preds += pred.view(-1).tolist()
                 running_loss += loss.item() * batch_size
 
-
             epoch_loss = running_loss / len(data_sets[phase])
             epoch_f1 = f1_score(labels, preds)
             epoch_accuracy = accuracy_score(labels, preds)
@@ -150,15 +159,17 @@ def train(model, data_sets, optimizer, num_epochs: int, batch_size=16):
 
 
 def main():
-    for model_name in ["glove-wiki-gigaword-50", "glove-wiki-gigaword-100", "glove-wiki-gigaword-200"]:
-        model = downloader.load(model_name)
-        print(f"Model: {model_name}")
-        train_set = NerDataset("train.tagged", None, model, model_name)
-        test_set = NerDataset("dev.tagged", None, model, model_name)
+    model2 = downloader.load('word2vec-google-news-300')
+    for model_name in ['glove-twitter-25', 'glove-twitter-50', 'glove-twitter-100', 'glove-twitter-200','word2vec-google-news-300']:
+        model1 = downloader.load(model_name)
+        model2_name = 'word2vec-google-news-300'
+        print(f"glove Model: {model_name}, word2vec Model: {model2_name}")
+        train_set = NerDataset("train.tagged", None, model1, model2, model_name, model2_name)
+        test_set = NerDataset("dev.tagged", None, model1, model2, model_name, model2_name)
         nn_model = NerNN(num_classes=2, vec_dim=train_set.vector_dim)
-        optimizer = Adam(params=nn_model.parameters())
+        optimizer = Adagrad(params=nn_model.parameters())
         datasets = {"train": train_set, "dev": test_set}
-        train(model=nn_model, data_sets=datasets, optimizer=optimizer, num_epochs=5)
+        train(model=nn_model, data_sets=datasets, optimizer=optimizer, num_epochs=7)
         print()
 
 
